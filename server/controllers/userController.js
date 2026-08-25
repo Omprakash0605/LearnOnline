9ffda84200b7585b89e93498a9ac415fedb7b1ab
Om Prakash Dash <omprakash74152@gmail.com>
2026-08-25 17:39:49 +0530

    courserouter added

---
+++ server/controllers/userController.js
@@ -0,0 +1,31 @@
+import User from "../models/User.js"
+import { getAuth } from "@clerk/express"
+
+//get user data
+export const getUserData = async(req, res)=>{
+    try {
+        const { userId } = getAuth(req);
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.json({success: false, message: 'User Not Found'})
+        }
+
+        res.json({success:true, user})
+    } catch (error) {
+        res.json({success:false, message: error.message})
+    }
+}
+
+//users enrolled courses ith lecturelinks
+export const userEnrolledCourses = async (req, res)=>{
+    try{
+        const {usedId} = getAuth(req);
+        const userData = await User.findById(userId).populate('enrolledCourses')
+
+        res.json({success: true, enrolledCourses: userData.enrolledCourses})
+    }catch(error){
+        res.json({success: false, message:error.message})
+    }
+}
+
