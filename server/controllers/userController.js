3bedbbf8f075cbcfc6f730ec423ab1c6513ca95a
Om Prakash Dash <omprakash74152@gmail.com>
2026-09-06 13:43:28 +0530

    add-rating feature added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -77,4 +77,38 @@ export const getUserCourseProgress = async(req,res) => {
     }
 }
 
-// Add user ratings 
+// Add user ratings to course 
+export const addUserRating = async ()=>{
+    const { userId } = getAuth(req);
+    const { courseId, rating } = req.body;
+
+    if(!courseId || !userId || !rating || rating<1 || rating>5){
+        return res.json({success: false, message: 'Invalid deatails'});
+    }
+
+    try{
+        const course = await Course.findById(courseId);
+        if(!course){
+            return res.json({success: false, message: 'Course not found'});
+        }
+
+        const user = await User.findById(userId);
+        if(!user || !user.enrolledCourses.includes(courseId)){
+            return res.json({success: false, message: 'User has not purchase this course.'});
+        }
+
+        const existingRatingIndex = course.courseRatings.findIndex(r => r.userId === userId)
+
+        if(existingRatingIndex > -1){
+            course.courseRatings[existingRatingIndex].rating = rating;
+        }else{
+            course.courseRatings.push({userId, rating});
+        }
+        await course.save();
+
+        return res.json({success: true, message: 'Rating added'})
+
+    }catch(error){
+        return res.json({success: false, message:error.message});
+    }
+}
